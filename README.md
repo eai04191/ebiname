3b93dfe52a8a65a158ee91b2d60baf50683de096
Aoi Irie <eai@mizle.net>
2019-09-06 06:43:58 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+![](https://rawcdn.githack.com/eai04191/ebiname/a1fcf1c28e8ce640d25da71ecdaeb2d03f9da461/header.jpg)
+
+# ebiname
+
+<p>
+<img src="https://rawcdn.githack.com/eai04191/ebiname/a1fcf1c28e8ce640d25da71ecdaeb2d03f9da461/shrimp.svg" height="20">
+<img src="https://rawcdn.githack.com/eai04191/ebiname/a1fcf1c28e8ce640d25da71ecdaeb2d03f9da461/straight_shrimp.svg" height="20">
+<img src="https://rawcdn.githack.com/eai04191/ebiname/a1fcf1c28e8ce640d25da71ecdaeb2d03f9da461/win98_shrimp.svg" height="20">
+</p>
+
+## 免責事項
+
+[GitHubのヘルプ](https://help.github.com/ja/articles/about-github-actions)に
+
+> また、次の目的ではGitHub Actionsを使用しないでください。  
+> （中略）  
+> 
+> - サーバーレスコンピューティング  
+> 
+> （中略）  
+> GitHub Actionsを不正利用した場合には、ジョブが停止されたり、GitHub Actionsの使用を制限されたりすることもあります。
+
+とあります。
+
+このリポジトリでの用途は思いっきりサーバーレスコンピューティングなので、これを使用したことによりアカウントのActionsの使用を制限されるなどしても、作者は一切の責任を負いません。
+
+## LICENSE
+
+MIT
---
+++ src/index.js
@@ -0,0 +1,40 @@
+const axios = require("axios");
+const MersenneTwister = require("mersenne-twister");
+
+const host = process.env.MASTODON_HOST;
+const token = process.env.MASTODON_TOKEN;
+const name = process.env.MASTODON_NAME;
+
+const ebiList = ["🦐", ":straight_shrimp:", ":win98_shrimp:"];
+const zwnbsp = String.fromCharCode(parseInt("0xFEFF", 16));
+
+const body = {
+    display_name:
+        name +
+        zwnbsp +
+        getRandomEbi(ebiList) +
+        zwnbsp +
+        getRandomEbi(ebiList) +
+        zwnbsp +
+        getRandomEbi(ebiList)
+};
+
+console.log(body);
+
+const client = axios.create({
+    baseURL: `https://${host}/api/v1/`,
+    headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+    }
+});
+
+client
+    .patch(`accounts/update_credentials`, body)
+    .then(res => console.log(res.status))
+    .catch(err => console.error(err));
+
+function getRandomEbi(array) {
+    const generator = new MersenneTwister();
+    return array[Math.floor(generator.random_incl() * array.length)];
+}
