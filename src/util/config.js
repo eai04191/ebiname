a2e2fe230202ae1d8c78985009801787a68aa268
y.takahashi <eai@mizle.net>
2023-02-27 22:05:14 +0900

    add evirified and evirifried

--- src/util/config.js
+++ src/util/config.js
@@ -5,5 +5,11 @@ exports.botToken = process.env.MASTODON_BOT_TOKEN;
 exports.GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY;
 exports.GITHUB_RUN_ID = process.env.GITHUB_RUN_ID;
 
-exports.ebiList = ["🦐", ":straight_shrimp:", ":win98_shrimp:"];
+exports.ebiList = [
+  "🦐",
+  ":straight_shrimp:",
+  ":win98_shrimp:",
+  ":evirified:",
+  ":evirifried:",
+];
 exports.zwnbsp = String.fromCharCode(parseInt("0xFEFF", 16));
